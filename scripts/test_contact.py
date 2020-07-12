93ce3baa3005e50d05c8659e04f71e9087c7a5b4
Ruby-xiaodong <2399455065@qq.com>
2020-07-12 13:35:44 +0800

    Jenkins实验

---
+++ scripts/test_contact.py
@@ -0,0 +1,18 @@
+class TestContact:
+
+    def test01(self):
+        assert 1
+
+    def test02(self):
+        assert 1
+
+    def test03(self):
+        assert 0
+
+    def test04(self):
+        assert 1
+
+    def test06(self):
+        assert 1
+
+
